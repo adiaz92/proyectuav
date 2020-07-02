e896020958815d1e3e2aaf73dfe5026acfab4432
adiaz92 <shaenar9@gmail.com>
2020-07-02 19:40:31 +0200

    Update Mission Planner

--- src/mission_planner/src/mission_planner.cpp
+++ src/mission_planner/src/mission_planner.cpp
@@ -1,1 +1,13 @@
 #include "mission_planner.h"
+
+mission::missionNode()
+{
+  nh.getParam("parcel_dispatcher/check_pad_position", checkpad);
+  nh.getParam("parcel_dispatcher/charging_pad_pose", chargingpad);
+  nh.getParam("parcel_dispatcher/shelve_pose", shelve);
+  nh.getParam("parcel_dispatcher/husky_pose", husky);
+
+
+
+
+}
